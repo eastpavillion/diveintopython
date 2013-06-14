90f78b00f3b6d695ac3a3b9d7285c42244758c39
Odeussea <lorrainemei@gmail.com>
2013-06-14 22:49:05 +0800

    add if_statement function
    add if = main sentence as unit test

---
+++ if_statement.py
@@ -0,0 +1,16 @@
+__author__ = 'lorrainemei'
+
+def fib(n):
+    print "n=",n
+    if n > 1:
+        return n * fib(n-1)
+    else:
+        print 'end of the line'
+        return 1
+
+
+if __name__ == "__main__":
+    fib(5)
+
+
+
