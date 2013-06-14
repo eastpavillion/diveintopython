6d54afde8f2d46008ba9c0f0300de555e5c7d883
Odeussea <lorrainemei@gmail.com>
2013-06-14 22:24:33 +0800

    add odbchelper python

---
+++ odbchelper.py
@@ -0,0 +1,17 @@
+__author__ = 'lorrainemei'
+
+
+def buildConnectionString(params):
+    """Build a connection string from a dictionary of parameters.
+
+    Return string."""
+    return ";".join(["%s=%s" % (k , v) for k , v in params.items()])
+
+if __name__ == "__main__":
+    myParams = { "server":"mpilgrim",\
+                 "database":"master",\
+                 "uid":"sa",\
+                 "pwd":"secrete" \
+                }
+    print buildConnectionString(myParams)
+
